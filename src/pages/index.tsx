44ef228f776f64bb126988707523e82e209350f0
tseijp <t1810394@edu.cc.uec.ac.jp>
2022-03-02 14:54:10 +0900

    :zap:feat: Migrate from cra to docusaurus

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react';
+import Head from '@docusaurus/Head'
+import Layout from '@theme/Layout';
 import { Text } from "@react-three/drei";
 import { useMove } from "@use-gesture/react";
 import { Instanced, Flow } from "react-mol";
 import { Canvas, useFrame } from "@react-three/fiber";
+import type { DocusaurusConfig } from '@docusaurus/types';
 
 const { sin, cos, random } = Math;
 
@@ -12,6 +14,7 @@ const position = (t = 0, s = 0, x = 0, y = 0, z = 0) => [
   (y - 0.5 - sin(t * s + y) - cos((t * s) / 3)) * (y * 100 + 50),
   (z - 0.5 - cos(t * s + z) - sin((t * s) / 5)) * (z * 100 + 50)
 ];
+
 const rotation = (t = 0, s = 0) => Array(3).fill(cos(t * s) * 5);
 
 function Group() {
@@ -36,19 +39,25 @@ function Group() {
           key={i}
           color="black"
           args={[...Array(4)].map(random)}
-          position={position}
-          rotation={rotation}
-          scale={rotation}
+          position={position as any}
+          rotation={rotation as any}
+          scale={rotation as any}
         />
       ))}
     </Instanced>
   );
 }
 
-function App() {
+export default function App (props: {config: DocusaurusConfig}) {
+  const { config: siteConfig } = props
   return (
-    <React.StrictMode>
-      <Canvas camera={[0, 0, 5]} style={{ top: 0, left: 0, position: "fixed" }}>
+    <Layout noFooter>
+      <Head>
+        <title>
+          {siteConfig.title} {siteConfig.titleDelimiter} {siteConfig.tagline}
+        </title>
+      </Head>
+      <Canvas camera={[0, 0, 5] as any} style={{ top: 0, left: 0, zIndex: -1, position: "absolute" }}>
         <ambientLight intensity={0.3} />
         <color attach="background" args={["#212121"]} />
         <gridHelper position={[0, 0, 0]} args={[100, 50]} />
@@ -59,8 +68,6 @@ function App() {
           TSEI.JP
         </Text>
       </Canvas>
-    </React.StrictMode>
+    </Layout>
   );
 }
-
-ReactDOM.render(<App />, document.getElementById("root"));
